da8c785f6b62cb2e065fda87ba27229cba8a40cc
Merilee Wheelock <eelirem@gmail.com>
2017-07-06 11:25:13 -0400

    sql updates

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,10 +1,37 @@
+// EXPRESS SERVER
+
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	host: '127.0.0.1',
+	user: 'x',
+	password: 'x',
+	database: 'todo'
+})
 
+connection.connect();
+
+// Set up a route to handle react's first request
 router.get('/getStudents', function(req, res, next) {
-	res.json({
-		msg: "Success!"
+	connection.query('SELECT * FROM students', (error, results)=>{
+		if (error) throw error;
+		res.json(results);
+	});
+});
+
+// addStudent route. Expects a name in the body, will add that name to the students table,
+// then respond with all students in that table
+router.post('/addStudent', (req, res)=>{
+	var studentToAdd = req.body.name;
+	connection.query('INSERT INTO students (name) VALUES (?)', [studentToAdd], (error, results)=>{
+		if (error) throw error;
+		connection.query('SELECT * FROM students', (error2, results2)=>{
+			if (error2) throw error2;
+			res.json(results2);
+		});
 	})
+	// res.json({msg: "test"})
 });
 
 module.exports = router;
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -9,22 +9,58 @@ class App extends Component {
 		this.state = {
 			theClass: []
 		}
+        // Make sure addStudent uses the correct "this"
+        this.addStudent = this.addStudent.bind(this);
 	}
 
+    // componentDidMount runs after the first render
     componentDidMount() {
+        // getJSON request to localhost:3000, that's where express is listening
         $.getJSON('http://localhost:3000/getStudents', (studentsFromApi)=>{
+            // log the JSON response from express
             console.log(studentsFromApi);
+            this.setState({
+                theClass: studentsFromApi
+            })
         });
-        this.setState({
-            theClass: [1,2,3,4]
+        // Update the state... this will cause a re-render
+        // this.setState({
+        //     theClass: [1,2,3,4]
+        // })
+    }
+
+    addStudent(event){
+        var studentToAdd = event.target.parentNode.childNodes[0].value;
+        // var studentToAdd = document.getElementById('newStudent');
+        // console.log(studentToAdd);
+        // This is a POST request, so we can't use $.getJSON (only does get)
+        // $.ajax expects an object that tells it what to send (data), where to send it (url),
+        // and how tot send it (method)
+        // $.ajax is a promose which has a "done" method that will run when ajax is back
+        // It gets a param of whatever SON was returned by the API request
+        // In side that function, we update React state (theClass), which causes a rerender,
+        // which updates the list because we're mapping through state
+        $.ajax({
+            method: "POST",
+            url: "http://localhost:3000/addStudent",
+            data: {
+                name: studentToAdd
+            }
+        }).done((studentsArray)=>{
+            this.setState({
+                theClass: studentsArray
+            })
         })
     }
 
 	render() {
 
+        // Create an array to dump into our return. It will contain components or HTML tags
         var theClassArray = [];
+        // Loop through out state var.The first time through, it will be empty.
         this.state.theClass.map((student, index)=>{
-            theClassArray.push(<li key={index}>Student</li>)
+            // Push an li tag onto our array for each element in the state variable
+            theClassArray.push(<li key={index}>{student.name}</li>)
         });
 
 		return (
@@ -36,6 +72,10 @@ class App extends Component {
 				<p className="App-intro">
 					To get started, edit <code>src/App.js</code> and save to reload.
 				</p>
+                <div className="add-box">
+                    <input type="text" id="newStudent" placeholder="Add Student" />
+                    <button onClick={this.addStudent}>Submit</button>
+                </div> 
                 <p>
                     {theClassArray}
                 </p>
